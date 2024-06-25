aaa14fae6cd798bb4420d2b1feb2891e2da13736
Pabalberione <pabalberione@gmail.com>
2024-06-25 20:54:26 -0300

    create-unit-tests ready

--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -1,2 +1,6 @@
 public class SimpleCalculator {
+    //Create method
+    public int add(int numberA, int numberB){
+        return numberA + numberB;
+    }
 }
---
+++ src/test/java/GraderTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraderTest {
+
+    @Test
+    void fiftyNineShouldReturnF(){
+        var grader = new Grader();
+        assertEquals('F', grader.determineLetterGrade(59));
+    }
+
+    @Test
+    void sixtyNineShouldReturnD(){
+        var grader = new Grader();
+        assertEquals('D', grader.determineLetterGrade(69));
+    }
+
+    @Test
+    void seventyNineShouldReturnC(){
+        var grader = new Grader();
+        assertEquals('C', grader.determineLetterGrade(79));
+    }
+
+    @Test
+    void eightyNineShouldReturnB(){
+        var grader = new Grader();
+        assertEquals('B', grader.determineLetterGrade(89));
+    }
+
+    @Test
+    void ninetyOneShouldReturnA(){
+        var grader = new Grader();
+        assertEquals('A', grader.determineLetterGrade(91));
+    }
+
+    @Test
+    void negativeOneShouldReturnIllegalArgumentException(){
+        var grader = new Grader();
+        assertThrows(IllegalArgumentException.class, () -> {
+            grader.determineLetterGrade(-1);
+        });
+
+    }
+
+}
